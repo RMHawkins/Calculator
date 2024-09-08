289d98f2856953085546a9f303931b4425f45b7d
RMHawkins <reubenmhawkins@gmail.com>
2024-09-08 11:56:38 -0500

    Implemented subraction feature

--- calculator.c
+++ calculator.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int addition(int a, int b);
+int subtraction(int a, int b);
 
 void main(){
- 
- }
+    
+}
 
 int addition(int a, int b){
     return a + b;
+}
+
+int subtraction(int a, int b){
+    return a - b;
 }
