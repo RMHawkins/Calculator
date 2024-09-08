8ef172bdd9ea3ac0bf5b6176de2246d19dfd40c7
RMHawkins <reubenmhawkins@gmail.com>
2024-09-08 14:13:58 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -3,6 +3,7 @@
 int addition(int a, int b);
 int subtraction(int a, int b);
 int multiplication(int a, int b);
+int division(int a, int b);
 
 void main(){
     
@@ -18,4 +19,12 @@ int subtraction(int a, int b){
 
 int multiplication(int a, int b){
     return a * b;
+}
+int division(int a, int b){
+    // follows method of a / b
+    if( b == 0){
+        printf("Divide by zero error\n");
+        return 0;
+    }
+    else return a/b;
 }
